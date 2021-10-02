1f5ff41a05cb04fd4bfb8121e8d71fcbfe0f08e5
Thiago <tbgdmelo@gmail.com>
2021-10-01 22:19:36 -0400

    anterada a porta

--- index.js
+++ index.js
@@ -47,7 +47,5 @@ app.use("/js", [
 app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
-app.listen(3000, function(){
-    console.log("express iniciado na porta 3000");
-})
+app.listen(process.env.PORT || 3000)
 
